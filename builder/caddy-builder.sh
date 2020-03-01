190e5b2c6b0e07e724850049e9026aaa75002ba8
Dave Henderson <dhenderson@gmail.com>
2020-03-01 13:15:52 -0500

    Use caddy-builder script
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

---
+++ builder/caddy-builder.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+plugins=$@
+
+mkdir -p /src/custom-caddy/cmd/caddy
+cd /src/custom-caddy/cmd/caddy
+
+cat >> main.go <<EOF
+package main
+
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+EOF
+
+for p in $plugins; do
+    echo -e "\t_ \"$p\"" >> main.go
+done
+
+cat >> main.go <<EOF
+)
+
+func main() {
+	caddycmd.Main()
+}
+EOF
+
+cat >> go.mod <<EOF
+module caddy
+
+go 1.14
+
+require (
+	github.com/caddyserver/caddy/v2 v2.0.0-beta.15
+)
+EOF
+
+CGO_ENABLED=0 go build -trimpath -tags netgo -ldflags '-extldflags "-static" -s -w' -o /usr/bin/caddy
