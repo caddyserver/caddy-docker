a54f7d1165d6240f53984ad42475ac833b2e6562
Dave Henderson <dhenderson@gmail.com>
2020-09-24 20:27:55 -0400

    Deprecate the caddy-builder.sh script
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- 2.1/builder/caddy-builder.sh
+++ 2.1/builder/caddy-builder.sh
@@ -6,6 +6,12 @@ for p; do
 	args="$args --with $p"
 done
 
+echo "Warning: the caddy-builder script is deprecated and will be removed in the future.
+Instead, you should use the xcaddy command:
+
+    xcaddy build $args
+" >&2
+
 # version is inferred from $CADDY_VERSION (set in the Dockerfile)
 # output will be placed in the working dir (/usr/bin as set in the Dockerfile)
 xcaddy build $args
